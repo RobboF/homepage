f5366928e61cdf9049d7d1a08ecc079197edb72b
Robbo  🦄 <Rob.fisher@outlook.com>
2020-04-19 15:15:01 +0100

    test

--- data/src/components/header/header.js
+++ data/src/components/header/header.js
@@ -4,7 +4,7 @@ export default function header() {
     return (
         <header className="tc pv4 pv5-ns">
             <img src={logo} className="br-100 pa1 ba b--black-10 h4 w4" alt="avatar"/>
-            <h1 className="mt2 mb0 baskerville i fw1 f1">Robbo.xyz</h1>
+            <h1 className="mt2 mb0 baskerville i fw1 f1">- Robbo.xyz -</h1>
             <h2 className="mt2 mb0 f6 fw4 ttu tracked">My humble corner of the internet</h2>
             <nav className="bt bb tc mw7 center mt4">
                 <a className="f6 f5-l link bg-animate black-80 hover-bg-lightest-blue dib pa3 ph4-l" href="/">Home</a>
