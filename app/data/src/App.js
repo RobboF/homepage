b0e0a736b676c721b49887027d9d1904b40deea3
RobboF <rob.fisher@outlook.com>
2021-02-22 00:08:58 +0000

    finished new look

--- app/data/src/App.js
+++ app/data/src/App.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react'
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Index from './pages/index/index'
-import About from './pages/about/about'
-import Portfolio from './pages/portfolio/portfolio'
-import Social from './pages/social/social'
-import './App.css';
 
-export default class App extends Component {
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faTwitter, faGithubSquare, faDocker } from "@fortawesome/free-brands-svg-icons"
 
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Router>
-        <Route path="/" exact render={() => (<Index/>)} />
-        <Route path="/about" exact component={About} />
-        <Route path="/portfolio" exact component={Portfolio}  /> 
-        <Route path="/contact" exact component={Social}  /> 
-      </Router>
-    )
-  }
+function App() {
+  return (
+    <article style={{fontFamily: "Lato"}}>
+      <div
+        class="vh-100 dt w-100 tc bg-dark-gray white cover"
+        // style={{ backgroundImage: `url(http://mrmrs.github.io/photos/u/009.jpg)`, backgroundRepeat: "no-repeat" }}
+      >
+        <div class="dtc v-mid">
+          <h1 class=" f-headline-l fw1 white-60">Robbo.xyz</h1>
+          <div class=" measure center flex justify-around w-100">
+            <FontAwesomeIcon className="white-60" icon={faLinkedin} size="5x"/>
+            <FontAwesomeIcon className="white-60" icon={faTwitter} size="5x"/>
+            <FontAwesomeIcon className="white-60" icon={faGithubSquare} size="5x"/>
+            <FontAwesomeIcon className="white-60" icon={faDocker} size="5x"/>
+          </div>
+        </div>
+      </div>
+    </article>
+  );
 }
 
-
+export default App;
