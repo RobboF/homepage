a325a63919d38242c0ef5a0622f9a24169a92dc0
RobboF <rob.fisher@outlook.com>
2021-02-25 21:58:57 +0000

    Added logo and links

--- app/data/src/App.js
+++ app/data/src/App.js
@@ -1,21 +1,46 @@
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faLinkedin, faTwitter, faGithubSquare, faDocker } from "@fortawesome/free-brands-svg-icons"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faLinkedin,
+  faTwitter,
+  faGithubSquare,
+  faDocker,
+} from "@fortawesome/free-brands-svg-icons";
+import { ReactComponent as Logo } from "./assets/images/logo.svg";
 function App() {
   return (
-    <article style={{fontFamily: "Lato"}}>
+    <article style={{ fontFamily: "Lato" }}>
       <div
         class="vh-100 dt w-100 tc bg-dark-gray white cover"
         // style={{ backgroundImage: `url(http://mrmrs.github.io/photos/u/009.jpg)`, backgroundRepeat: "no-repeat" }}
       >
         <div class="dtc v-mid">
+          <Logo fill="#ffffff30" height="25vh" />
           <h1 class=" f-headline-l fw1 white-60">Robbo.xyz</h1>
           <div class=" measure center flex justify-around w-100">
-            <FontAwesomeIcon className="white-60" icon={faLinkedin} size="5x"/>
-            <FontAwesomeIcon className="white-60" icon={faTwitter} size="5x"/>
-            <FontAwesomeIcon className="white-60" icon={faGithubSquare} size="5x"/>
-            <FontAwesomeIcon className="white-60" icon={faDocker} size="5x"/>
+            <a href="https://www.linkedin.com/in/robbo-fisher/" title="LinkedIn">
+              <FontAwesomeIcon
+                className="white-60"
+                icon={faLinkedin}
+                size="5x"
+              />
+            </a>
+            <a href="https://twitter.com/RobboRob" title="Twitter">
+              <FontAwesomeIcon
+                className="white-60"
+                icon={faTwitter}
+                size="5x"
+              />
+            </a>
+            <a href="https://github.com/RobboF" title="Github">
+              <FontAwesomeIcon
+                className="white-60"
+                icon={faGithubSquare}
+                size="5x"
+              />
+            </a>
+            <a href="https://hub.docker.com/u/robbof" title="DockerHub">
+              <FontAwesomeIcon className="white-60" icon={faDocker} size="5x" />
+            </a>
           </div>
         </div>
       </div>
