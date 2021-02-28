8488b4e8479c1441d6e96f8f4777dd6eb07d8613
RobboF <rob.fisher@outlook.com>
2021-02-28 13:39:35 +0000

    Added a shitty test

--- app/data/src/App.js
+++ app/data/src/App.js
@@ -2,16 +2,39 @@ import { ReactComponent as Logo } from "./assets/images/logo.svg";
 import { useMediaQuery } from "react-responsive";
 import Icons from "./Icons";
 import IconsMobile from "./IconsMobile";
-
+import {
+  faLinkedin,
+  faTwitter,
+  faGithubSquare,
+  faDocker,
+} from "@fortawesome/free-brands-svg-icons";
 const App = () => {
   const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
+  const iconArr = [
+    { site: "Twitter", url: "https://twitter.com/RobboRob", icon: faTwitter },
+    {
+      site: "LinkedIn",
+      url: "https://www.linkedin.com/in/robbo-fisher/",
+      icon: faLinkedin,
+    },
+    {
+      site: "Dockerhub",
+      url: "https://hub.docker.com/u/robbof",
+      icon: faDocker,
+    },
+    {
+      site: "Github",
+      url: "https://github.com/RobboF",
+      icon: faGithubSquare,
+    },
+  ];
   return (
     <article style={{ fontFamily: "Lato" }}>
-      <div class="vh-100 dt w-100 tc bg-dark-gray white cover">
-        <div class="dtc v-mid">
+      <div className="vh-100 dt w-100 tc bg-dark-gray white cover">
+        <div className="dtc v-mid">
           <Logo fill="#ffffff60" height={isTabletOrMobile ? "90vw" : "500px"} />
-          <h1 class=" f-headline-1-ns f-subheadline fw1 white-60">Robbo.xyz</h1>
-         {isTabletOrMobile ? <IconsMobile/> : <Icons/>} 
+          <h1 className=" f-headline-1-ns f-subheadline fw1 white-60">Robbo.xyz</h1>
+         {isTabletOrMobile ? <IconsMobile iconArr={iconArr}/> : <Icons iconArr={iconArr}/>} 
         </div>
       </div>
     </article>
--- app/data/src/Icons.js
+++ app/data/src/Icons.js
@@ -1,40 +1,14 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faLinkedin,
-  faTwitter,
-  faGithubSquare,
-  faDocker,
-} from "@fortawesome/free-brands-svg-icons";
 
-export default function Icons() {
-  const iconArr = [
-    { site: "Twitter", url: "https://twitter.com/RobboRob", icon: faTwitter },
-    {
-      site: "LinkedIn",
-      url: "https://www.linkedin.com/in/robbo-fisher/",
-      icon: faLinkedin,
-    },
-    {
-      site: "Dockerhub",
-      url: "https://hub.docker.com/u/robbof",
-      icon: faDocker,
-    },
-    {
-      site: "Github",
-      url: "https://github.com/RobboF",
-      icon: faGithubSquare,
-    },
-  ];
+export default function Icons(props) {
   return (
-    <div class=" measure center flex flex-wrap justify-around w-60">
-      {
-          iconArr.map((el) => (
-            <a href={el.url} title={el.site}>
-              <FontAwesomeIcon icon={el.icon} size="5x" color="#ffffff30" />
-            </a>
-          ))
-      }
+    <div className=" measure center flex flex-wrap justify-around w-60">
+      {props.iconArr.map((el, idx) => (
+        <a href={el.url} title={el.site} key={`icon-${idx}`}>
+          <FontAwesomeIcon icon={el.icon} size="5x" color="#ffffff30" />
+        </a>
+      ))}
     </div>
   );
 }
