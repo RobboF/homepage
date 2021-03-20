13d56b63f3412ec5594c62e44c3ae0e1d1626312
RobboF <rob.fisher@outlook.com>
2021-03-20 13:35:18 +0000

    Fixed Dockerfile

--- app/data/components/App.js
+++ app/data/components/App.js
@@ -1,5 +1,4 @@
 import Logo from "../public/logo.svg";
-import { useMediaQuery } from "react-responsive";
 import Icons from "./Icons";
 import {
   faLinkedin,
@@ -8,7 +7,6 @@ import {
   faDocker,
 } from "@fortawesome/free-brands-svg-icons";
 const App = () => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
   const iconArr = [
     { site: "Twitter", url: "https://twitter.com/RobboRob", icon: faTwitter },
     {
@@ -31,7 +29,7 @@ const App = () => {
     <article style={{ fontFamily: "Lato" }}>
       <div className="vh-100 dt w-100 tc bg-dark-gray white cover">
         <div className="dtc v-mid">
-          <Logo fill="#ffffff60" height={isTabletOrMobile ? "90vw" : "500px"} />
+          <Logo fill="#ffffff60" height={"500px"} />
           <h1 className=" f-headline-1-ns f-subheadline fw1 white-60">Robbo.xyz</h1>
          <Icons iconArr={iconArr}/>
         </div>
--- app/data/pages/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders App name', () => {
-  render(<App />);
-  const linkElement = screen.getByText("Robbo.xyz");
-  expect(linkElement).toBeInTheDocument();
-});
